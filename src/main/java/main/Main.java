30c79a17527a292bc002de1eff015edca6b79c82
Simon Olsson <ndi16son@student.hig.se>
2018-11-21 11:24:13 +0100

    aa

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -19,7 +19,7 @@ import static spark.Spark.*;
  * Main klass som initierar spark samt skapar default smtp.json config fil om den ej finns
  */
 public class Main {
-    // Sebastian was here
+    // Sebastian was heree
     public static void main(String[] args) throws Exception {
         MailSenderProvider.createDefaultConfFileIfNotExists();
         initSpark();
